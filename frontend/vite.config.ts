1a5ec990ecb9fc21cedaef9bbaa439cacbcd1b20
David <davidmm1107@hotmail.com>
2026-02-15 21:03:21 -0500

    fix: fixing frontend test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,8 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.ts'
+    setupFiles: './src/test/setup.ts',
+    //threads: !process.env.CI,
+    //pool: 'forks',
   }
 })
